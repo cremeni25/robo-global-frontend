829f62b6a0ab0a2fc2b81c3a640fbc25a577c924
cremeni25 <andersonnavarro.si@gmail.com>
2026-01-21 02:12:50 -0300

    Create NotAuthorized.tsx

---
+++ apps/web/_future/b2/pages/NotAuthorized.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function NotAuthorized() {
+  return (
+    <div>
+      <h2>Acesso não autorizado</h2>
+      <p>Você não possui permissão para acessar esta área.</p>
+    </div>
+  );
+}
