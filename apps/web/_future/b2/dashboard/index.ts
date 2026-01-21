75f16705db2e5402186d65134405023ad44d7f17
cremeni25 <andersonnavarro.si@gmail.com>
2026-01-21 02:26:22 -0300

    Update index.ts

--- apps/web/_future/b2/dashboard/index.ts
+++ apps/web/_future/b2/dashboard/index.ts
@@ -1,4 +1,5 @@
-// Entry point da B2
-// Nenhuma exportação para o build principal
+// apps/web/_future/b2/dashboard/index.ts
 
-export {};
+export { default as ProtectedDashboard } from './ProtectedDashboard';
+export { default as DashboardB2 } from './Dashboard';
+export { NotAuthorized } from './NotAuthorized';
