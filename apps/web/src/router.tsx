c92ce4c970031d1369632008b99e99320def5a1a
cremeni25 <andersonnavarro.si@gmail.com>
2026-01-26 00:21:56 -0300

    Update router.tsx

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -1,1 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LayoutGlobal from "./layout/LayoutGlobal";
+import Home from "./pages/Home";
+import Nichos from "./pages/Nichos";
+import Dores from "./pages/Dores";
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <LayoutGlobal>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/nichos" element={<Nichos />} />
+          <Route path="/dores" element={<Dores />} />
+          <Route path="/sobre" element={<div>Sobre</div>} />
+        </Routes>
+      </LayoutGlobal>
+    </BrowserRouter>
+  );
+}
 
