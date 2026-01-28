4efef5e96b1922bea0650e12d5c0c8e37a5fbad3
cremeni25 <andersonnavarro.si@gmail.com>
2026-01-28 08:29:37 -0300

    Update main.tsx

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { LanguageProvider } from "./contexts/LanguageContext";
 
 const rootElement = document.getElementById("root");
 
@@ -10,6 +11,8 @@ if (!rootElement) {
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
   </React.StrictMode>
 );
