f153757766d7f871ce19fb7cb2d9dff0194cb3df
cremeni25 <andersonnavarro.si@gmail.com>
2026-02-13 13:26:10 -0300

    Update router.tsx

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -4,20 +4,29 @@ import LayoutGlobal from "./components/LayoutGlobal";
 
 import Home from "./pages/Home";
 import Nichos from "./pages/Nichos";
+import NichosPublic from "./pages/NichosPublic";
 import Dores from "./pages/Dores";
 import Sobre from "./pages/Sobre";
 import Dashboard from "./pages/dashboard";
+import Go from "./pages/Go";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <LayoutGlobal>
         <Routes>
+          {/* ROTAS GLOBAIS */}
           <Route path="/" element={<Home />} />
           <Route path="/nichos" element={<Nichos />} />
+          <Route path="/nichos-public" element={<NichosPublic />} />
           <Route path="/dores" element={<Dores />} />
           <Route path="/sobre" element={<Sobre />} />
+
+          {/* DASHBOARD MASTER */}
           <Route path="/dashboard" element={<Dashboard />} />
+
+          {/* REDIRECIONADOR INTELIGENTE */}
+          <Route path="/go/:slug" element={<Go />} />
         </Routes>
       </LayoutGlobal>
     </BrowserRouter>
