fcd4075b6dccad8167dc40e36a69ef32bcfc3451
cremeni25 <andersonnavarro.si@gmail.com>
2026-02-13 13:44:19 -0300

    Update router.tsx

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -1,25 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import LayoutGlobal from "./layout/LayoutGlobal";
+
 import Home from "./pages/Home";
 import Nichos from "./pages/Nichos";
 import Dores from "./pages/Dores";
-import CMS from "./pages/CMS";
+import Dashboard from "./pages/dashboard";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-
-        {/* ROTAS COM HEADER + LAYOUT */}
         <Route element={<LayoutGlobal />}>
           <Route path="/" element={<Home />} />
           <Route path="/nichos" element={<Nichos />} />
           <Route path="/dores" element={<Dores />} />
+          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/sobre" element={<div>Sobre</div>} />
-          <Route path="/secure/cms" element={<CMS />} />
         </Route>
-
       </Routes>
     </BrowserRouter>
   );
