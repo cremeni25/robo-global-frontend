a8b836c9f078800be2a0e925d25da1f484817fc4
cremeni25 <andersonnavarro.si@gmail.com>
2026-02-13 13:49:20 -0300

    Update router.tsx

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import LayoutGlobal from "./layout/LayoutGlobal";
 
 import Home from "./pages/Home";
 import Nichos from "./pages/Nichos";
 import Dores from "./pages/Dores";
+import Sobre from "./pages/Sobre";
 import Dashboard from "./pages/dashboard";
 
 export default function Router() {
@@ -15,8 +15,8 @@ export default function Router() {
           <Route path="/" element={<Home />} />
           <Route path="/nichos" element={<Nichos />} />
           <Route path="/dores" element={<Dores />} />
+          <Route path="/sobre" element={<Sobre />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/sobre" element={<div>Sobre</div>} />
         </Route>
       </Routes>
     </BrowserRouter>
