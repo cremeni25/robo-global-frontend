da1d8ddce4e6af2e3d5f391f7b2730d87b258ca8
cremeni25 <andersonnavarro.si@gmail.com>
2026-02-13 12:52:41 -0300

    Update router.tsx

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -1,23 +1,54 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LayoutGlobal from "./layout/LayoutGlobal";
 
 import Home from "./pages/Home";
 import Nichos from "./pages/Nichos";
+import NichosPublic from "./pages/NichosPublic";
 import Dores from "./pages/Dores";
-import Sobre from "./pages/Sobre";
-import Dashboard from "./pages/dashboard";
+import CMS from "./pages/CMS";
 import Go from "./pages/Go";
+import Dashboard from "./pages/dashboard";
+import NichosPublic from "./pages/NichosPublic";
+import CMS from "./pages/CMS";
+import Sobre from "./pages/Sobre";
+import Aguarde from "./pages/Aguarde";
 
 export default function Router() {
-  return (
-    <BrowserRouter>
+return (
+<BrowserRouter>
+      <LayoutGlobal>
+        <Routes>
+          <Route path="/" element={<Home />} />
       <Routes>
+
+        {/* 🌎 Público Global */}
         <Route path="/" element={<Home />} />
-        <Route path="/nichos" element={<Nichos />} />
-        <Route path="/dores" element={<Dores />} />
-        <Route path="/sobre" element={<Sobre />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/nichos" element={<NichosPublic />} />
+        <Route path="/nicho/:id" element={<Nichos />} />
+        <Route path="/dores/:id" element={<Dores />} />
         <Route path="/go/:slug" element={<Go />} />
+
+        {/* 🧠 MASTER */}
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/cms" element={<CMS />} />
+
+          {/* CAMADA PÚBLICA */}
+          <Route path="/nichos-public" element={<NichosPublic />} />
+        {/* 📘 Institucional */}
+        <Route path="/sobre" element={<Sobre />} />
+
+          {/* CAMADA INTERNA */}
+          <Route path="/nichos" element={<Nichos />} />
+          <Route path="/dores" element={<Dores />} />
+          <Route path="/secure/cms" element={<CMS />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        {/* ⏳ fallback */}
+        <Route path="*" element={<Aguarde />} />
+
+          <Route path="/sobre" element={<div>Sobre</div>} />
+        </Routes>
+      </LayoutGlobal>
       </Routes>
-    </BrowserRouter>
-  );
+</BrowserRouter>
+);
 }
