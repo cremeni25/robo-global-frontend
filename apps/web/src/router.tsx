b472cd3683afda7a25c47883013d7ef62a73b3a4
cremeni25 <andersonnavarro.si@gmail.com>
2026-02-13 13:38:21 -0300

    Update router.tsx

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -1,40 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import LayoutGlobal from "./components/LayoutGlobal";
-
+import LayoutGlobal from "./layout/LayoutGlobal";
 import Home from "./pages/Home";
 import Nichos from "./pages/Nichos";
-import NichosPublic from "./pages/NichosPublic";
 import Dores from "./pages/Dores";
-import Sobre from "./pages/Sobre";
-import Dashboard from "./pages/dashboard";
-import Go from "./pages/Go";
-
-function AppRoutes() {
-  return (
-    <Routes>
-      {/* ROTAS GLOBAIS */}
-      <Route path="/" element={<Home />} />
-      <Route path="/nichos" element={<Nichos />} />
-      <Route path="/nichos-public" element={<NichosPublic />} />
-      <Route path="/dores" element={<Dores />} />
-      <Route path="/sobre" element={<Sobre />} />
-
-      {/* DASHBOARD MASTER */}
-      <Route path="/dashboard" element={<Dashboard />} />
-
-      {/* REDIRECIONADOR */}
-      <Route path="/go/:slug" element={<Go />} />
-    </Routes>
-  );
-}
+import CMS from "./pages/CMS";
 
 export default function Router() {
   return (
     <BrowserRouter>
-      <LayoutGlobal>
-        <AppRoutes />
-      </LayoutGlobal>
+      <Routes>
+
+        {/* ROTAS COM HEADER + LAYOUT */}
+        <Route element={<LayoutGlobal />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/nichos" element={<Nichos />} />
+          <Route path="/dores" element={<Dores />} />
+          <Route path="/sobre" element={<div>Sobre</div>} />
+          <Route path="/secure/cms" element={<CMS />} />
+        </Route>
+
+      </Routes>
     </BrowserRouter>
   );
 }
