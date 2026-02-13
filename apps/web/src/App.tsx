5995e3f15489f4b8fdeb442fd4b4607b9e40c326
cremeni25 <andersonnavarro.si@gmail.com>
2026-02-13 13:17:49 -0300

    Update App.tsx

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,10 +1,5 @@
 import Router from "./router";
-import LayoutGlobal from "./layout/LayoutGlobal";
 
 export default function App() {
-  return (
-    <LayoutGlobal>
-      <Router />
-    </LayoutGlobal>
-  );
+  return <Router />;
 }
