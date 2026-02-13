5d7994256e277461792eae47f07dbaaf10ee5906
cremeni25 <andersonnavarro.si@gmail.com>
2026-02-13 12:32:14 -0300

    Update router.tsx

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -1,31 +1,38 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LayoutGlobal from "./layout/LayoutGlobal";
+
 import Home from "./pages/Home";
 import Nichos from "./pages/Nichos";
-import Dores from "./pages/Dores";
-import CMS from "./pages/CMS";
-import Dashboard from "./pages/dashboard";
 import NichosPublic from "./pages/NichosPublic";
+import Dores from "./pages/Dores";
+import Go from "./pages/Go";
+import Dashboard from "./pages/dashboard";
+import CMS from "./pages/CMS";
+import Sobre from "./pages/Sobre";
+import Aguarde from "./pages/Aguarde";
 
 export default function Router() {
   return (
     <BrowserRouter>
-      <LayoutGlobal>
-        <Routes>
-          <Route path="/" element={<Home />} />
+      <Routes>
 
-          {/* CAMADA PÚBLICA */}
-          <Route path="/nichos-public" element={<NichosPublic />} />
+        {/* 🌎 Público Global */}
+        <Route path="/" element={<Home />} />
+        <Route path="/nichos" element={<NichosPublic />} />
+        <Route path="/nicho/:id" element={<Nichos />} />
+        <Route path="/dores/:id" element={<Dores />} />
+        <Route path="/go/:slug" element={<Go />} />
 
-          {/* CAMADA INTERNA */}
-          <Route path="/nichos" element={<Nichos />} />
-          <Route path="/dores" element={<Dores />} />
-          <Route path="/secure/cms" element={<CMS />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+        {/* 🧠 MASTER */}
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/cms" element={<CMS />} />
 
-          <Route path="/sobre" element={<div>Sobre</div>} />
-        </Routes>
-      </LayoutGlobal>
+        {/* 📘 Institucional */}
+        <Route path="/sobre" element={<Sobre />} />
+
+        {/* ⏳ fallback */}
+        <Route path="*" element={<Aguarde />} />
+
+      </Routes>
     </BrowserRouter>
   );
 }
